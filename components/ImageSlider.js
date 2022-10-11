f38f90c94391756e57f49a6f1b53281f363cf066
Daniel Baramo <96897948+dbaramo@users.noreply.github.com>
2022-10-10 22:28:57 -0400

    added image slider

--- components/ImageSlider.js
+++ components/ImageSlider.js
@@ -1,13 +1,26 @@
 import styles from "./ImageSlider.module.css";
+import buildings from "../data/data.json";
+import { useState, useEffect, useRef } from "react";
 
-const imagesList = [
-    
-]
+const imagesForSlider = buildings.map((b) => b.images[0])
 
 export default function ImageSlider(){
+    const [imgIndex, setImgIndex] = useState(0);
+    const divElement = useRef(null);
+    
+    useEffect(() => {
+        if(imgIndex >= 12) setImgIndex(0)
+        divElement.current.style.backgroundImage = `url(${imagesForSlider[imgIndex]})`
+    }, [imgIndex])
+    setTimeout(() => setImgIndex(imgIndex + 1), 3000)
+
     return (
-        <div className={styles.divStyle}>
-            <p>image slider section here</p>
+        <div 
+        style={{
+            backgroundImage: `url(${imagesForSlider[0]})`
+        }} className={styles.divStyle}
+        ref={divElement}
+        >
         </div>
         )
 }
