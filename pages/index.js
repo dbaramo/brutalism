e4bd6602f5bfd9b0b13ca3585d168a135fb6965b
Daniel Baramo <96897948+dbaramo@users.noreply.github.com>
2022-10-12 14:34:07 -0400

    preloaded all images

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,18 @@ import Image from 'next/image'
 import Intro from '../components/Intro.js'
 import ImageSlider from '../components/ImageSlider.js'
 import BuildingList from '../components/BuildingList.js'
+import buildings from '../data/data.json'
 
+const buildingImages = []
+buildings.forEach((b) => buildingImages.push(...b.images))
 
 export default function Home() {
   return (
     <div className='mainDiv'>
     <Head>
-    <link rel="preload" as="image" href="/habitat-67/img-01.jpeg" />
+    {buildingImages.map((img) => {
+      return <link key={img} rel="preload" as="image" href={img} />
+    })}
     </Head>
       <section className={styles.main}>
         <Intro />
