3ba6f71d64e38890605e1c03621b6cfeb593c102
Daniel Baramo <96897948+dbaramo@users.noreply.github.com>
2022-10-18 20:11:22 -0400

    building page adjustments

--- components/BuildingsPage/BuildingPageLayout.js
+++ components/BuildingsPage/BuildingPageLayout.js
@@ -1,23 +1,54 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./BuildingPage.module.css";
+import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 
 export default function BuildingPageLayout(props) {
   let { architect, building, city, country, description, images } = props.data;
-  console.log(gsap);
+  const [imgIndex, setImgIndex] = useState(0);
+  const imgsDiv = useRef(null);
+
+  useEffect(() => {
+    // -- ANIMATION CODE HERE --
+    let ctx = gsap.context(() => {
+      let targets = gsap.utils.toArray(".building-imgs");
+
+      // gsap.to(".building-imgs", {
+      //   y: "-=500",
+      //   duration: 5
+      // })
+    }, imgsDiv);
+  }, []);
+
+  function nextImage() {
+    if (imgIndex === images.length - 1) {
+      setImgIndex(0);
+    } else {
+      setImgIndex(imgIndex + 1);
+    }
+  }
+
+  function prevImage() {
+    if (imgIndex === 0) {
+      setImgIndex(images.length - 1);
+    } else {
+      setImgIndex(imgIndex - 1);
+    }
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.leftDiv}>
-        <div>
+        <div className={styles.metaData}>
           <h1>{building}</h1>
           <span>Location: </span>
           <span>
             {city}, {country}
           </span>
         </div>
-        <div>
-          <span>Architect:</span>
+        <div className={styles.metaData}>
+          <span>Architect: </span>
           <span>{architect}</span>
         </div>
         <div className={styles.description}>
@@ -27,12 +58,26 @@ export default function BuildingPageLayout(props) {
           <button>Home</button>
         </Link>
       </div>
-      <div className={styles.rightDiv}>
-        {images.map((url) => {
-          return(
-            <img src={url} key={url} />
-          )
-        })}
+      <div ref={imgsDiv} className={styles.rightDiv}>
+        <div className={styles.frame}>
+          <img className="building-imgs" src={images[imgIndex]} />
+          <div className={styles.arrowsDiv}>
+            <img
+              onClick={prevImage}
+              className={styles.leftArrow}
+              src="/arrow-left-black.svg"
+              width={26}
+              height={20}
+            />
+            <img
+              onClick={nextImage}
+              className={styles.rightArrow}
+              src="/arrow-right-black.svg"
+              width={26}
+              height={20}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
