96d94a02924e74b340a5ec77d74dfe8eaf6b62aa
Daniel Baramo <96897948+dbaramo@users.noreply.github.com>
2022-11-17 10:31:11 -0500

    fix quotations error

--- components/Intro.js
+++ components/Intro.js
@@ -4,10 +4,10 @@ export default function Intro(){
             <p>Brutalism is a style of architecture that lasted from the 1950s-1970s, 
             and it was characterized by simple, block-like, hulking concrete structures. 
             It originated in England and spread to the rest of the world shortly after.
-            The term Brutalism—coined by Swedish architect Hans Asplund as "nybrutalism" 
+            The term Brutalism—coined by Swedish architect Hans Asplund as `&quot;`nybrutalism`&quot;`
             and popularized by British architectural critic Reyner Banham in 1955—is not a reference 
             to the arguably brutal nature of its appearance but a play on the French phrase for raw concrete, 
-            "béton brut."
+            `&quot;`béton brut.`&quot;`
             </p>
         </div>
         )
